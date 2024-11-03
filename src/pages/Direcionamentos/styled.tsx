eeaf4ec5dde560eb4e720b5457dccdf7fe5ed9e6
1King-coder <progvitor@gmail.com>
2024-11-02 22:09:55 -0300

    Direcionamentos styled-components

---
+++ src/pages/Direcionamentos/styled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import * as colors from "../../config/colors";
+
+export const Title = styled.h1`
+  color: ${colors.secondaryColor};
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 50px;
+  text-align: center;
+  background-color: ${colors.primaryColor};
+  padding: 10px;
+  width: 100%;
+`
