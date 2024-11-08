3a7036015fca46437f79a53470f3cffca659f3df
1King-coder <progvitor@gmail.com>
2024-11-08 14:24:23 -0300

    added style classes for popovers

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -64,6 +64,32 @@ export default createGlobalStyle`
     padding: 10px;
   }
 
+  .gastos-gerais-popover-content {
+    width: 100%; 
+    padding: 10px; 
+    height: 100%; 
+    background-color: ${tertiaryColor}; 
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    border-radius: 20px;
+  }
+
+  .gastos-gerais-popover-title-div {
+    width: 100%;
+    height: 100%; 
+    display: flex; 
+    justify-content: center; 
+    background-color: ${primaryColor}; 
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    border-radius: 20px;
+  }
+
+  .gastos-gerais-popover-title {
+    color: ${secondaryColor};
+    text-align: center; 
+    font-size: 14px; 
+    padding: 10px;
+    font-weight: bold;
+  }
 `
 
 export const Container = styled.section`
