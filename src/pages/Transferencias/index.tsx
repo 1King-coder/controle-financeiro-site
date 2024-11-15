4b27507d2620d7045b9acf0c6e3d573179132536
1King-coder <progvitor@gmail.com>
2024-11-15 12:49:11 -0300

    added type Transferencia and changed tipo transferencia selection

--- src/pages/Transferencias/index.tsx
+++ src/pages/Transferencias/index.tsx
@@ -6,39 +6,47 @@ import { StyledButton, StyledButtonGroup } from "../../styles/GlobalStyles";
 
 class getTransferencias {
 
-  static async getTransferenciasEntreBancos() {
-    const res = await axios.get("/transferencias_entre_bancos");
+  static async getTransferencias(tipoTransferenciaUrlPath: string) {
+    const res = await axios.get(tipoTransferenciaUrlPath);
     return res.data;
   }
 
-  static async getTransferenciasEntreDirecionamentos() {
-    const res = await axios.get("/transferencias_entre_direcionamentos");
-    return res.data;
-  }
 }
 
 export default function Transferencias() {
   const [transferencias, setTransferencias] = React.useState([]);
   const [optionSelectedId, setOptionSelectedId]: [number, any] = React.useState(1);
+  const [transferenciasUrlPath, setTransferenciasUrlPath]: [string, any] = React.useState("/transferencias_entre_bancos");
 
 
 
   React.useEffect(() => {
-    getTransferencias.getTransferenciasEntreBancos().then((data) => {
+    getTransferencias.getTransferencias(transferenciasUrlPath).then((data) => {
       console.log(data);
       setTransferencias(data);
     });
-  }, []);
+  }, [transferenciasUrlPath]);
+
+  React.useEffect(() => {
+    if (optionSelectedId === 1) {
+      setTransferenciasUrlPath("/transferencias_entre_bancos");
+    } else {
+      setTransferenciasUrlPath("/transferencias_entre_direcionamentos");
+    }
+  }, [optionSelectedId]);
 
 
   return (
     <div className="transferencias-body-div">
       <Title>Transferencias</Title>
-
       <StyledButtonGroup className="transferencias-button-group">
         <StyledButton id={1} selected={optionSelectedId === 1} onClick={() => setOptionSelectedId(1)}>Entre Bancos</StyledButton>
         <StyledButton id={2} selected={optionSelectedId === 2} onClick={() => setOptionSelectedId(2)}>Entre Direcionamentos</StyledButton>
       </StyledButtonGroup>
+      <div>
+        
+      </div>
+
     </div>
   );
 }
---
+++ src/types/Transferencia.ts
@@ -0,0 +1,8 @@
+export type Transferencia = {
+  id: number;
+  id_origem: number;
+  id_destino: number;
+  valor: number;
+  descricao: string;
+  created_at: string;
+}
