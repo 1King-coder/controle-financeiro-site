1dac6ca4249bd863b682e7c215e1a6f8a8754665
1King-coder <progvitor@gmail.com>
2024-11-16 19:03:39 -0300

    changed site primary colors

--- src/config/colors.tsx
+++ src/config/colors.tsx
@@ -1,13 +1,13 @@
 
 
-export const primaryColor = '#520c9c';
-export const lightPrimartyColor = '#633297';
+export const primaryColor = '#130779';
+export const lightPrimartyColor = '#281d86';
 export const secondaryColor = '#fcad03';
-export const successColor = '#5122a8';
+export const successColor = '#2232c0';
 export const warningColor = '#ed7300';
 export const dangerColor = '#fa3737';
 export const backgroundColor = '#212121';
 export const tertiaryColor = '#faf7f7';
-export const selectedBGColor = '#27064b'
+export const selectedBGColor = '#0f075a'
 export const selectedFontColor = '#ffce63'
 export const CardTitleColor = '#ebe8e8'
