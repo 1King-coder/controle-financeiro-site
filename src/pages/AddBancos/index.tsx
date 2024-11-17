b000860f26ffd7f3192968c367eccb63b32eadf4
1King-coder <progvitor@gmail.com>
2024-11-17 10:06:59 -0300

    created adding Bancos fforms

--- src/pages/AddBancos/index.tsx
+++ src/pages/AddBancos/index.tsx
@@ -1,6 +1,21 @@
 import React from "react";
-import { Title } from "./styled";
+import { GeneralBox, InputBox } from "./styled";
+import { Title } from "../../styles/GlobalStyles";
+import { Button, Label, TextInput } from "flowbite-react";
 
 export default function AddBancos() {
-  return <Title>AddBancos</Title>;
+  return (
+    <GeneralBox>
+      <Title>Adicionar Bancos</Title>
+      <form>
+        <InputBox>
+          <Label htmlFor="nome-banco" value="Nome do banco"/>
+          <TextInput id="nome-banco" placeholder="Nome do banco"/>
+        </InputBox>
+        <InputBox>
+          <Button>Adicionar</Button>
+        </InputBox>
+      </form>
+    </GeneralBox>
+  );
 }
--- src/pages/AddBancos/styled.tsx
+++ src/pages/AddBancos/styled.tsx
@@ -1,5 +1,53 @@
 import styled from "styled-components";
+import * as colors from "../../config/colors";
 
-export const Title = styled.h1`
-  color: black;
+
+export const GeneralBox = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  form {
+    margin: 1rem auto;
+    background-color: ${colors.tertiaryColor};
+    padding: 2rem;
+    border-radius: 1rem;
+    border: 3px solid ${colors.primaryColor};
+
+  }
+`
+
+export const InputBox = styled.div`
+  margin: 1rem auto;
+  text-align: center;
+
+  Label {
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+
+  #nome-banco {
+    font-size: 1rem;
+    margin-top: 0.5rem;
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+    border: 2px solid ${colors.primaryColor};
+  }
+
+  Button {
+    padding: 1rem;
+    border-radius: 0.5rem;
+    border: 2px solid ${colors.primaryColor};
+    background-color: ${colors.primaryColor};
+    color: ${colors.secondaryColor};
+    font-size: 1rem;
+    font-weight: bold;
+    width: 100%;
+    :hover {
+      opacity: 0.7;
+    }
+  }
+
+
 `
