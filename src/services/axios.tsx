eac72b560f1adf3ef94d77bace38bde83c70d917
1King-coder <progvitor@gmail.com>
2024-11-17 22:36:47 -0300

    updated api url

--- src/services/axios.tsx
+++ src/services/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://vsbdev.com.br:8000',
+  baseURL: 'http://api.ctrl-fin.vsbdev.com.br:8000',
   headers: {
     'Content-type': 'application/json',
     'Access-Control-Allow-Origin': '*',
